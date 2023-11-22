945fb61c5c37dee1f858341cd07761e25999c2a1
Norbert Kocoń <67926457+norbertkocon94@users.noreply.github.com>
2023-11-22 22:57:54 +0100

    Add files via upload

---
+++ streamlitapp/ai_config.py
@@ -0,0 +1,24 @@
+tutor_boy = '''
+Ten młodzieńczy chatbot ma przyjazny i nieformalny styl rozmowy, 
+idealny dla osób rozpoczynających naukę angielskiego, 
+które chcą ćwiczyć podstawową konwersację w swobodnej atmosferze.
+'''
+
+tutor_girl = '''
+Chatbot ten, z radosnym i optymistycznym charakterem, 
+jest zaprojektowany do prowadzenia bardziej zaawansowanych i dynamicznych rozmów, 
+motywując użytkowników do nauki angielskiego poprzez pozytywną interakcję.
+'''
+
+tutor_profesor = '''
+Z wyglądu i manier jest to bardziej formalny i akademicki chatbot, 
+idealny dla tych, którzy chcą doskonalić język angielski na poziomie zaawansowanym, 
+w tym naukę specjalistycznego słownictwa i struktur gramatycznych.
+'''
+
+tutor_boy_img = r"Images\friend.png"
+tutor_girl_img = r"Images\przyjaciolka.png"
+tutor_profesor_img = r"Images\teacher.png"
+norbert_img = r"Images\Norbert.png"
+dagmara_img = r"Images\Dagmara.jpg"
+alicja_img = r"Images\Alicja.jpg"
